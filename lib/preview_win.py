0be6d80d4a6eff02a8941f78386e7dc1daee16be
Daniel Hackworth <dan.hackworth.515@gmail.com>
2023-11-30 11:15:09 -0500

    finalized gui

--- lib/preview_win.py
+++ lib/preview_win.py
@@ -6,11 +6,20 @@ def show_preview(pic):
 
     root = Tk()  # create a root widget
     root.title(f"{pic.title}")
-    
+    def get_image_dimensions(path):
+        try:
+            with Image.open(path) as img:
+                width, height = img.size
+                return width, height
+        except IOError as e:
+            print(f"Error opening the image: {e}")
+            return None
     #may have to change path
     path = f"./{pic.preview}"
     img = ImageTk.PhotoImage(file=path)
-    my_canvas = Canvas(root, height=600, width=600)
+    width, height = get_image_dimensions(path)
+    root.geometry(f"{width}x{height}")
+    my_canvas = Canvas(root, height=height, width=width)
     my_canvas.pack(fill="both", expand=True)
     
     my_canvas.create_image(0,0, image=img, anchor="nw")
@@ -26,7 +35,7 @@ def show_preview(pic):
         root.destroy()
         #root.quit()
     
-   
+
     root.bind('<Configure>', resizer)
    
     root.mainloop()
